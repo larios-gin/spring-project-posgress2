1e3a528a13c00abec031189b81363c8573723bd0
larios-gin <roger.lario.7e3@itb.cat>
2021-04-29 20:46:01 +0200

    prova 3

--- src/main/java/cat/itb/springproject/model/Pokemon.java
+++ src/main/java/cat/itb/springproject/model/Pokemon.java
@@ -10,24 +10,57 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Pokemon {
 
     @Id
     private String nomPokemon;
 
-    @NotNull
-    @NotEmpty
     private int numPokemon;
 
-    @NotNull
-    @NotEmpty
     private String tipusPokemon;
 
-    @NotNull
-    @NotEmpty
     private String descripcioPokemon;
+
+    public Pokemon(String nomPokemon, int numPokemon, String tipusPokemon, String descripcioPokemon) {
+        this.nomPokemon = nomPokemon;
+        this.numPokemon = numPokemon;
+        this.tipusPokemon = tipusPokemon;
+        this.descripcioPokemon = descripcioPokemon;
+    }
+
+    public Pokemon() {
+    }
+
+    public String getNomPokemon() {
+        return nomPokemon;
+    }
+
+    public void setNomPokemon(String nomPokemon) {
+        this.nomPokemon = nomPokemon;
+    }
+
+    public int getNumPokemon() {
+        return numPokemon;
+    }
+
+    public void setNumPokemon(int numPokemon) {
+        this.numPokemon = numPokemon;
+    }
+
+    public String getTipusPokemon() {
+        return tipusPokemon;
+    }
+
+    public void setTipusPokemon(String tipusPokemon) {
+        this.tipusPokemon = tipusPokemon;
+    }
+
+    public String getDescripcioPokemon() {
+        return descripcioPokemon;
+    }
+
+    public void setDescripcioPokemon(String descripcioPokemon) {
+        this.descripcioPokemon = descripcioPokemon;
+    }
 }
